b46b0551c099368821023e2c0502d04cac9c32a1
Ryan Zacharias <swatdem12@yahoo.ca>
2022-10-03 21:06:55 -0700

    reverted unnecessary javascript nonsense

--- generator.js
+++ generator.js
@@ -1,11 +1,25 @@
 const pug = require('pug');
 const fs = require('fs');
+var apikey = null;
+fs.readFile('./env/apikey.txt', (err, data) => {
+    if(err) throw err;
+    apikey = data;
+});
+
+
+
+
+
+
+
+
+
 
 const index = pug.compileFile('./index.pug');
 const gallery = pug.compileFile('./gallery.pug');
-const data = index({});
 
-fs.writeFile('index.html', data, (err) => {
+//create html files
+fs.writeFile('index.html', index({}), (err) => {
     if (err) throw err;
 });
 
