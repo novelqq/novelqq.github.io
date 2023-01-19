0bf56d78fb1d7a79a99b1dfab779b57d20e94614
Ryan Zacharias <zacharias.ryan@gmail.com>
2023-01-18 23:12:27 -0800

    index template done

--- build/builder.py
+++ build/builder.py
@@ -1,23 +1,39 @@
-from markdown import markdown
+import markdown
 from glob import glob
 
 
-blog_posts = []
 
 def get_posts():
     posts_path = 'posts/{}.md'
     posts = glob(posts_path.format('*'))
-    
+    blog_posts = []
     for i in range(len(posts)):
         file = open(posts_path.format(i), 'r')
-        title = file.readline().strip()
-        content = markdown(file.read().strip())
-        blog_posts.append({"title": title, "content": content}) 
+        data = file.read().strip()
+        md = markdown.Markdown(extensions=['meta'])
+        html = md.convert(data)
+        print(md.Meta)
+        blog_posts += f"<li><a href=\"posts/{i}\">{title}</li>\n"
+        print(blog_posts)
         file.close()
-    return
+    return blog_posts
 
 
-def write_html():
+def write_html(blog_posts_html):
     with open("template.html", "r") as template:
-        output = template.read().replace('<!-- CONTENT -->', content)
-         
+        output = template.read().replace('<!-- CONTENT -->', blog_posts_html )
+        o = open("../index.html", "w")
+        o.write(output)
+        o.close()
+        return
+
+def create_index():
+    pass
+
+def create_post_pages():
+    pass
+
+def 
+if __name__ == "__main__":
+    blog_posts_html = get_posts()
+    write_html(blog_posts_html)
